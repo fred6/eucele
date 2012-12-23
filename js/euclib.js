61a5752d3f4a59262f5eb3d48899a2d5d34d620d
fred6 <frexids@gmail.com>
2012-12-23 02:50:13 -0500

    remove the  chunk of code i duplicated last commit (whee), rename public to pub, add a semicolon (JS Hint fixes)

--- js/euclib.js
+++ js/euclib.js
@@ -5,34 +5,25 @@
     // when it's not there)
     var Raphael = global.Raphael;
 
-    // for now we require a div with id canvas to be setup on HTML page
     // but this is very much a temporary thing until I understand a better
     // way to pass this in
     var r = Raphael("canvas", 600, 400);
 
-    var public = {};
+    var pub = {};
 
     /*** Objects ***/
 
-    // but this is very much a temporary thing until I understand a better
-    // way to pass this in
-    var r = Raphael("canvas", 600, 400);
-
-    var public = {};
-
-    /*** Objects ***/
-
-    public.Point = function(x, y) {
+    pub.Point = function(x, y) {
         this.x = x;
         this.y = y;
     };
 
-    public.Point.prototype.draw = function() {
+    pub.Point.prototype.draw = function() {
         if(!this.display)
             this.display = r.circle(this.x, this.y, 2).attr({fill: "#000"});
     };
 
-    public.Segment = function(pt1, pt2) {
+    pub.Segment = function(pt1, pt2) {
         // ugly, but some other logic depends on left-most being the first point
         if(pt1.x <= pt2.x) {
             this.A = pt1;
@@ -46,7 +37,7 @@
                               + Math.pow(this.B.y - this.A.y, 2));
     };
 
-    public.Segment.prototype.draw = function() {
+    pub.Segment.prototype.draw = function() {
         var pt2String = function(pt) {
             return pt.x+", "+pt.y;
         };
@@ -56,7 +47,7 @@
     };
 
     // get change from A to B
-    public.Segment.prototype.getChangePerLength = function(coord) {
+    pub.Segment.prototype.getChangePerLength = function(coord) {
         if(coord === "x") {
             return (this.B.x - this.A.x) / this.length;
         } else {
@@ -65,15 +56,15 @@
     };
 
     
-    public.Segment.prototype.drawA = function() {
+    pub.Segment.prototype.drawA = function() {
         this.A.draw();
     };
 
-    public.Segment.prototype.drawB = function() {
+    pub.Segment.prototype.drawB = function() {
         this.B.draw();
     };
 
-    public.Circle = function(point, rad) {
+    pub.Circle = function(point, rad) {
         this.center = point;
         this.cx = point.x;
         this.cy = point.y;
@@ -81,7 +72,7 @@
     };
 
 
-    public.Circle.prototype.draw = function() {
+    pub.Circle.prototype.draw = function() {
         if(!this.display)
             this.display = r.circle(this.cx, this.cy, this.rad);
     };
@@ -89,14 +80,14 @@
 
     // the idea is that instead of having a separate triangle object, im just going
     // to have a proposition function return a group of segments.
-    public.EleGroup = function(eles) {
+    pub.EleGroup = function(eles) {
         for(ele in eles) {
             this[ele] = eles[ele];
         }
     };
 
 
-    public.EleGroup.prototype.draw = function() {
+    pub.EleGroup.prototype.draw = function() {
         for(var ele in this) {
             if(this.hasOwnProperty(ele)) {
                 this[ele].draw();
@@ -108,11 +99,11 @@
     /*** Utility funcitons ***/
 
 
-    public.circFromSeg = function(segment, endpoint) {
+    pub.circFromSeg = function(segment, endpoint) {
         if(endpoint === "A") {
-            return new public.Circle(segment.A, segment.length);
+            return new pub.Circle(segment.A, segment.length);
         } else {
-            return new public.Circle(segment.B, segment.length);
+            return new pub.Circle(segment.B, segment.length);
         }
     };
 
@@ -120,7 +111,7 @@
     // that includes both the extension and the original segment
     //
     // direction = 1 means A --> B, -1 means B --> A
-    public.extendSegment = function(segment, endpoint, length, direction) {
+    pub.extendSegment = function(segment, endpoint, length, direction) {
         var segpt;
         if(endpoint === "A") {
             segpt = segment.A;
@@ -130,14 +121,14 @@
         
         var y_inc = direction * segment.getChangePerLength("y") * length;
         var x_inc = direction * segment.getChangePerLength("x") * length;
-        var extendpt = new public.Point(segpt.x + x_inc, segpt.y + y_inc);
+        var extendpt = new pub.Point(segpt.x + x_inc, segpt.y + y_inc);
 
-        return new public.Segment(segpt, extendpt)
+        return new pub.Segment(segpt, extendpt);
 
     };
 
 
-    public.findCircsIntersection = function(circ1, circ2) {
+    pub.findCircsIntersection = function(circ1, circ2) {
         var Lcirc, Rcirc;
 
         if(circ1.cx <= circ2.cx) {
@@ -148,7 +139,7 @@
             Rcirc = circ1;
         }
 
-        var btwn_centers = new public.Segment(Lcirc.center, Rcirc.center);
+        var btwn_centers = new pub.Segment(Lcirc.center, Rcirc.center);
 
         // x^2 + y^2 = R^2
         // (x-d)^2 + y^2 = r^2
@@ -183,14 +174,14 @@
         var cLineX = Lcirc.cx + x*Math.cos(cLineAng),
             cLineY = Lcirc.cy + x*Math.sin(cLineAng);
 
-        var cLinePt = new public.Point(cLineX, cLineY);
+        var cLinePt = new pub.Point(cLineX, cLineY);
 
         // finally get the coords of the top intersection (since there are
         // generally two)
         var interPtX = cLinePt.x + y*Math.sin(cLineAng),
             interPtY = cLinePt.y - y*Math.cos(cLineAng);
 
-        var interPt = new public.Point(interPtX, interPtY);
+        var interPt = new pub.Point(interPtX, interPtY);
 
         return interPt;
     };
@@ -198,14 +189,14 @@
 
     // finds intersection of circle and segment for a segment passing
     // through the center of the circle
-    public.findCircCenterSegIntersection = function(circ, seg) {
+    pub.findCircCenterSegIntersection = function(circ, seg) {
         // find change per length of the segment, then
         // multiply by the radius of the circle to find the change
         // from circle to intersection
         var interY = circ.cy + circ.rad * seg.getChangePerLength("y"),
             interX = circ.cx + circ.rad * seg.getChangePerLength("x");
 
-        return new public.Point(interX, interY);
+        return new pub.Point(interX, interY);
 
     };
 
@@ -216,15 +207,15 @@
 
     // Prop1 - takes a segment, returns an equilateral triangle with the segment
     // as one of the sides
-    public.Prop1 = function(seg) {
-        var c1 = public.circFromSeg(seg, "A"),
-            c2 = public.circFromSeg(seg, "B");
-        var inter = public.findCircsIntersection(c1,c2);
+    pub.Prop1 = function(seg) {
+        var c1 = pub.circFromSeg(seg, "A"),
+            c2 = pub.circFromSeg(seg, "B");
+        var inter = pub.findCircsIntersection(c1,c2);
 
-        var Aside = new public.Segment(seg.A, inter),
-            Bside = new public.Segment(seg.B, inter);
+        var Aside = new pub.Segment(seg.A, inter),
+            Bside = new pub.Segment(seg.B, inter);
 
-        return new public.EleGroup(
+        return new pub.EleGroup(
             {
                 orig: seg,
                 sideA: Aside,
@@ -236,21 +227,21 @@
 
     // Prop2 - takes a segment and a point, returns a segment located at the point
     // that's equal in length to the segment
-    public.Prop2 = function(seg, pt) {
-        var seg1 = new public.Segment(seg.B, pt);
+    pub.Prop2 = function(seg, pt) {
+        var seg1 = new pub.Segment(seg.B, pt);
 
-        var eqtri = public.Prop1(seg1);
+        var eqtri = pub.Prop1(seg1);
 
         // now make the circle based on the original given seg
-        var seg_circ = public.circFromSeg(seg, "B");
+        var seg_circ = pub.circFromSeg(seg, "B");
 
 
         //extend the other side of the equilateral triangle
-        var ext_seg = public.extendSegment(eqtri.sideA, "B", seg.length + 30, 1);
+        var ext_seg = pub.extendSegment(eqtri.sideA, "B", seg.length + 30, 1);
 
 
         //find intersection of circle and extended line
-        var interpt = public.findCircCenterSegIntersection(seg_circ, ext_seg);
+        var interpt = pub.findCircCenterSegIntersection(seg_circ, ext_seg);
 
         // the other point in the eq tri (not B or C)
         var eqtri_otherpt = eqtri.sideA.A;
@@ -258,22 +249,22 @@
 
         // line from other point of the eqtri to the intersection of the
         // extension and the circle
-        var ext_inter_seg = new public.Segment(eqtri_otherpt, interpt);
+        var ext_inter_seg = new pub.Segment(eqtri_otherpt, interpt);
 
-        var ext_inter_seg_circ = public.circFromSeg(ext_inter_seg, "A");
+        var ext_inter_seg_circ = pub.circFromSeg(ext_inter_seg, "A");
 
         // extend the remaining side of the eq tri
-        var last_ext_seg = public.extendSegment(eqtri.sideB, "B", seg.length + 30, 1) ;
+        var last_ext_seg = pub.extendSegment(eqtri.sideB, "B", seg.length + 30, 1) ;
 
         // find intersection of the newest circle and the last_ext_seg
-        var inter2pt = public.findCircCenterSegIntersection(ext_inter_seg_circ, last_ext_seg);
+        var inter2pt = pub.findCircCenterSegIntersection(ext_inter_seg_circ, last_ext_seg);
 
-        return new public.Segment(pt, inter2pt);
+        return new pub.Segment(pt, inter2pt);
 
     };
 
     // register globally
-    global.euclib = public;
+    global.euclib = pub;
 
 })(this);
 
