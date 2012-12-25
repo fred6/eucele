08a19fcab0667046a7bffa89c8514e8ce24ffe41
fred6 <frexids@gmail.com>
2012-12-25 01:23:22 -0500

    move hash set up code (grabbing unit based on hash, creating buttons and binding event handlers to the buttons that utilize the units) to a separate function in app.js. remove default buttons (so we dont get any unless we explicitly build them)

--- js/app.js
+++ js/app.js
@@ -12,9 +12,41 @@ define( ["jquery", "raphael", "euclib", "units"], function( $, Raphael, euclib, 
             u = units ( euc ),
             currUnit;
 
-        // Initially set highlight
+        // generic hash function, to be called either on hash change
+        // or on initial page load when a url with hash is loaded
+        function hashSetup ( name ) {
+            var currUnit;
+
+            if ( name !== "" ) {
+                currUnit = u [ name ];
+                currUnit.load();
+            }
+
+            // Set up buttons based on number of states in the unit
+            $("#unit-btn-grp button").remove();
+            var btn_txt;
+            for(var i = 0; i < currUnit.CG.numStates; i++) {
+                if ( i === 0 ) {
+                    btn_txt = '<button class="btn btn-block active">Given</button>';
+                } else {
+                    btn_txt = '<button class="btn btn-block">'+i+'</button>';
+                }
+
+                $(btn_txt).appendTo("#unit-btn-grp");
+
+            }
+
+            // delegated event handler for efficiency
+            $("#unit-btn-grp").on("click", "button", function(e) {
+                currUnit.goTo( $(this).index() );
+            });
+
+        }
+
+        // Setup based on initial hash
         var hash = location.hash.slice(1);
         setNavActive ( $('#units-nav > li > a[href = "#'+hash+'"]').parent() );
+        hashSetup ( hash );
 
         // Change nav-list highlights on click
         $("#units-nav").on("click", "li", function(e) {
@@ -26,10 +58,7 @@ define( ["jquery", "raphael", "euclib", "units"], function( $, Raphael, euclib, 
         $(window).bind('hashchange', function() {
             var name = location.hash.slice(1);
 
-            if ( name !== "" ) {
-                currUnit = u [ name ];
-                currUnit.load();
-            }
+            hashSetup ( name );
         });
 
 
